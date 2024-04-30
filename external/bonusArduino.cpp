00c66b09d2ee36a7f82e3035f775d040bf303813
Waddup778 <cameronabbey1@gmail.com>
2024-04-29 22:44:41 -0500

    Added bonusArduino

---
+++ external/bonusArduino.cpp
@@ -0,0 +1,77 @@
+#include <Arduino.h>
+#include <FastLED.h>
+
+// SETUP PIN CONNECTIONS TO THE ARDUINO
+const int ledPin = 10;
+const int pingPin = 11;
+
+// Define FastLED Class object
+#define NUM_LEDS 21
+CRGB leds[NUM_LEDS];
+
+// CREATE GLOBAL VARIABLES FOR THE LED ANIMATION FUNCTION TO USE
+unsigned long lastAnimationTime = 0;
+// SET THE ANIMATION TIME HERE *********************************
+unsigned long animationInterval = 1000;
+// *************************************************************
+
+void fadeAnimation(int red, int green, int blue) {
+    unsigned long animationStartTime = millis();
+    float r, g, b;
+    int elapsedTime = 0;
+
+    while (elapsedTime < 1024) {
+        elapsedTime = millis() - animationStartTime;
+        // Compact if-else statement using ?: notation
+        int brightness = (elapsedTime < 512) ? map(elapsedTime, 0, 512, 0, 255) : map(elapsedTime - 512, 0, 512, 255, 0);
+        r = (brightness / 255) * red;
+        g = (brightness / 255) * green;
+        b = (brightness / 255) * blue;
+        fill_solid(leds, NUM_LEDS, CRGB(r, g, b));
+        FastLED.show();
+    }
+}
+
+long microsecondsToInches(long microseconds) {
+    return microseconds / 74 / 2;
+}
+
+void handleLeds(int usPin) {
+    unsigned long currentMillis = millis();
+    long duration, inches;
+
+    pinMode(usPin, OUTPUT);
+    digitalWrite(usPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(usPin, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(usPin, INPUT);
+
+    duration = pulseIn(usPin, HIGH);
+
+    inches = microsecondsToInches(duration);
+
+    if (inches < 2 && currentMillis - lastAnimationTime >= animationInterval) {
+        lastAnimationTime = currentMillis;
+        fadeAnimation(255, 255, 255);
+    }   else if (inches >= 2) {
+        fill_solid(leds, NUM_LEDS, CRGB::Black);
+        FastLED.show();
+        lastAnimationTime = currentMillis;
+    }
+}
+
+void setup() {
+    pinMode(ledPin, OUTPUT);
+
+    Serial.println("Started!");
+    FastLED.setBrightness(50);
+    FastLED.addLeds<WS2812, ledPin, GRB>(leds, NUM_LEDS);
+
+    fill_solid(leds, NUM_LEDS, CRGB(255, 255, 255));
+    FastLED.show();
+}
+
+void loop() {
+    handleLeds(pingPin);
+}
---
+++ src/test.cpp
@@ -0,0 +1,6 @@
+#include "Arduino.h"
+#include "cocktailComposer.h"
+
+Drinks drinks;
+ManageLED led;
+
