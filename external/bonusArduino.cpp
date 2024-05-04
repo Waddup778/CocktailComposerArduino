2c6fde7b0ea173e5f0bc529ec93ffcda54bbd13e
Waddup778 <cameronabbey1@gmail.com>
2024-05-03 23:29:55 -0500

    Fixed Animation Error

--- external/bonusArduino.cpp
+++ external/bonusArduino.cpp
@@ -15,18 +15,35 @@ unsigned long lastAnimationTime = 0;
 unsigned long animationInterval = 1000;
 // *************************************************************
 
+// void fadeAnimation(int red, int green, int blue) {
+//     unsigned long animationStartTime = millis();
+//     float r, g, b;
+//     int elapsedTime = 0;
+
+//     while (elapsedTime < 1024) {
+//         elapsedTime = millis() - animationStartTime;
+//         // Compact if-else statement using ?: notation
+//         int brightness = (elapsedTime < 512) ? map(elapsedTime, 0, 512, 0, 255) : map(elapsedTime - 512, 0, 512, 255, 0);
+//         r = (brightness / 255) * red;
+//         g = (brightness / 255) * green;
+//         b = (brightness / 255) * blue;
+//         fill_solid(leds, NUM_LEDS, CRGB(r, g, b));
+//         FastLED.show();
+//     }
+// }
+
 void fadeAnimation(int red, int green, int blue) {
     unsigned long animationStartTime = millis();
     float r, g, b;
     int elapsedTime = 0;
 
-    while (elapsedTime < 1024) {
+    // Fade in and out combined
+    while (elapsedTime < 1024) { // Total duration of 512 + 512 milliseconds
         elapsedTime = millis() - animationStartTime;
-        // Compact if-else statement using ?: notation
         int brightness = (elapsedTime < 512) ? map(elapsedTime, 0, 512, 0, 255) : map(elapsedTime - 512, 0, 512, 255, 0);
-        r = (brightness / 255) * red;
-        g = (brightness / 255) * green;
-        b = (brightness / 255) * blue;
+        r = (brightness / 255.0) * red;
+        g = (brightness / 255.0) * green;
+        b = (brightness / 255.0) * blue;
         fill_solid(leds, NUM_LEDS, CRGB(r, g, b));
         FastLED.show();
     }
@@ -36,6 +53,30 @@ long microsecondsToInches(long microseconds) {
     return microseconds / 74 / 2;
 }
 
+// void handleLeds(int usPin) {
+//     unsigned long currentMillis = millis();
+//     long duration, inches;
+
+//     pinMode(usPin, OUTPUT);
+//     digitalWrite(usPin, LOW);
+//     delayMicroseconds(2);
+//     digitalWrite(usPin, HIGH);
+//     delayMicroseconds(5);
+//     digitalWrite(usPin, LOW);
+
+//     duration = pulseIn(usPin, HIGH);
+
+//     inches = microsecondsToInches(duration);
+
+//     if (inches < 2 && currentMillis - lastAnimationTime >= animationInterval) {
+//         lastAnimationTime = currentMillis;
+//         fadeAnimation(255, 255, 255);
+//     }   else if (inches >= 2) {
+//         fill_solid(leds, NUM_LEDS, CRGB::Black);
+//         FastLED.show();
+//         lastAnimationTime = currentMillis;
+//     }
+// }
 void handleLeds(int usPin) {
     unsigned long currentMillis = millis();
     long duration, inches;
@@ -45,16 +86,21 @@ void handleLeds(int usPin) {
     delayMicroseconds(2);
     digitalWrite(usPin, HIGH);
     delayMicroseconds(5);
-    digitalWrite(usPin, INPUT);
+    digitalWrite(usPin, LOW);
+
+    pinMode(usPin, INPUT);
 
     duration = pulseIn(usPin, HIGH);
 
     inches = microsecondsToInches(duration);
+    //Serial.println(inches);
 
     if (inches < 2 && currentMillis - lastAnimationTime >= animationInterval) {
         lastAnimationTime = currentMillis;
+        Serial.println("Object Detected.");
         fadeAnimation(255, 255, 255);
-    }   else if (inches >= 2) {
+    } else if (inches >= 2) {
+        Serial.println("No object detected.");
         fill_solid(leds, NUM_LEDS, CRGB::Black);
         FastLED.show();
         lastAnimationTime = currentMillis;
@@ -62,6 +108,8 @@ void handleLeds(int usPin) {
 }
 
 void setup() {
+    Serial.begin(9600);
+
     pinMode(ledPin, OUTPUT);
 
     Serial.println("Started!");
